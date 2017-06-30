fcca0e6bd4b87b5544bcf04e90bb2b9c20927855
John Ashcroft <scjea@foe-linux-02.leeds.ac.uk>
2017-06-30 11:47:06 +0100

    Fixed bugs and have first working figure of speed in m/s and storm_speed.py

--- storm_speed.py
+++ storm_speed.py
@@ -11,9 +11,11 @@ from convert_coordinates import find_centre
 from convert_coordinates import calculate_distance
 from plot_best_track import track_data
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from read_tctracker_data import *
 
 def calculate_speed(lat0, lat1, lon0, lon1, dt):
     dx = calculate_distance(lat0,lat1,lon0,lon1)
+    dx = dx*1000
     speed = dx / dt
     return speed
 
@@ -25,20 +27,23 @@ def make_same_size(array, sarray):
         sarray = np.append(sarray,0)
         return sarray
 
-def plot_storm_speed(speed,ft,lat,lon,monthday,times):
+def plot_storm_speed(speed,ft,lats,lons,monthday,times):
     lats = np.ma.masked_where(lats==0,lats)
     lons = np.ma.masked_where(lons==0,lons)
-    ft[:,1:-1] = np.ma.masked_where(ft[:,1:-1]==0,ft[:,1:-1])
-    speed[:,1:-1] = np.ma.masked_where(speed[:,1:-1]==0,speed[:,1:-1])
+    ft[:,1:] = np.ma.masked_less(ft[:,1:],0.01)
+    speed[:,1:] = np.ma.masked_less(speed[:,1:],0.01)
     colors = ['b','g','r','c','m','y','chocolate','b','g','r','c','m','y']
     linestyles = ['-','-','-','-','-','-','-','--','--','--','--','--','--']
-
     fig1 = plt.figure(1)
     ax1 = fig1.add_subplot(1,1,1)
     for i in np.arange(ft.shape[0]):
-        vt = (ft[0:-1]+ft[1:]) / 2.
-        ax1.plot(vt,speed[i,:],linewidth=2,linestyle=linestyles[i],color=colors[i])
-
+        # Ensure values which were appended with 0 are not included
+        if speed[i,-1] == 0:
+            vt = (ft[i][0:-2]+ft[i][1:-1]) / 2.
+            ax1.plot(vt,speed[i,:-1],linewidth=2,linestyle=linestyles[i],color=colors[i])
+        else:
+            vt = (ft[i][0:-1]+ft[i][1:]) / 2.
+            ax1.plot(vt,speed[i,:],linewidth=2,linestyle=linestyles[i],color=colors[i])
     plt.show()
 
 
@@ -51,21 +56,23 @@ def main():
     for md in monthday:
         for TT in times:
             if not (md == 1207 and TT == 12):
-                dataname = '/nfs/a37/scjea/suite-runs/Hagupit/data/tctracker_data/{0:04d}_{1:02d}Z'.format(md,hr)
+                dataname = '/nfs/a37/scjea/model_runs/Hagupit/data/tctracker_data/{0:04d}_{1:02d}Z'.format(md,TT)
                 ft1, omw1, lat1, lon1, dpe1, fvort1, fcp1, fmw1, rmw1 = read_tracker_output(dataname)
                 if i==0:
                     # Tracker code stops when storm reaches end of domain, hence for each time the arrays are of
                     # different sizes. ft0,lat0,lon0 keeps track of the size of original array.
                     ft, ft0 = ft1, ft1; lat, lat0 = lat1, lat1; lon, lon0 = lon1, lon1
+                    i = i + 1
                 else:
                     # make same size appends the array with a 0 which must be masked later on.
                     ft1 = make_same_size(ft0,ft1); lat1 = make_same_size(lat0,lat1); lon1 = make_same_size(lon0,lon1);
                     ft = np.vstack([ft,ft1]); lat = np.vstack([lat,lat1]); lon = np.vstack([lon,lon1])
+            
 
     # Now have 2D array of forecast times in hours and central positions of storms.
     for i in np.arange(ft.shape[0]):  #i.e. the number of forecasts (13 for Hagupit)
         k = 0
-        for j in np.arange(ft[0].shape[0] - 1): #i.e. the number of times in forecast
+        for j in np.arange(ft.shape[1] - 1): #i.e. the number of times in forecast
             lat0 = lat[i][j]; lat1 = lat[i][j+1];
             lon0 = lon[i][j]; lon1 = lon[i][j+1];
             dt = ft[i][j+1]-ft[i][j]
@@ -73,8 +80,9 @@ def main():
             new_speed = calculate_speed(lat0, lat1, lon0, lon1, dt)
             if k==0:
                 speed = new_speed
+                k = k+1
             else:
-                speed = np.append((speed,new_speed))
+                speed = np.append(speed,new_speed)
         # Account for cases where make_same_size has been used to sppend arrays
         if lon[i][-1] == 0:
             speed[-1] = 0

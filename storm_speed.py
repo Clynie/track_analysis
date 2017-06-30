a21cbe105d11ef711db8eab0b4fa3bd50de80918
John Ashcroft [RPG] <scjea@comp-pc3238.leeds.ac.uk>
2017-06-30 10:56:52 +0100

    First iteration, attempt of line graphs for storm speed

--- storm_speed.py
+++ storm_speed.py
@@ -12,7 +12,11 @@ from convert_coordinates import calculate_distance
 from plot_best_track import track_data
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
-def calculate_speed():
+def calculate_speed(lat0, lat1, lon0, lon1, dt):
+    dx = calculate_distance(lat0,lat1,lon0,lon1)
+    speed = dx / dt
+    return speed
+
 
 def make_same_size(array, sarray):
     if array.shape == sarray.shape:
@@ -20,6 +24,23 @@ def make_same_size(array, sarray):
     else:
         sarray = np.append(sarray,0)
         return sarray
+
+def plot_storm_speed(speed,ft,lat,lon,monthday,times):
+    lats = np.ma.masked_where(lats==0,lats)
+    lons = np.ma.masked_where(lons==0,lons)
+    ft[:,1:-1] = np.ma.masked_where(ft[:,1:-1]==0,ft[:,1:-1])
+    speed[:,1:-1] = np.ma.masked_where(speed[:,1:-1]==0,speed[:,1:-1])
+    colors = ['b','g','r','c','m','y','chocolate','b','g','r','c','m','y']
+    linestyles = ['-','-','-','-','-','-','-','--','--','--','--','--','--']
+
+    fig1 = plt.figure(1)
+    ax1 = fig1.add_subplot(1,1,1)
+    for i in np.arange(ft.shape[0]):
+        vt = (ft[0:-1]+ft[1:]) / 2.
+        ax1.plot(vt,speed[i,:],linewidth=2,linestyle=linestyles[i],color=colors[i])
+
+    plt.show()
+
 
 
 def main():
@@ -48,12 +69,21 @@ def main():
             lat0 = lat[i][j]; lat1 = lat[i][j+1];
             lon0 = lon[i][j]; lon1 = lon[i][j+1];
             dt = ft[i][j+1]-ft[i][j]
+            dt = dt * 60 * 60
             new_speed = calculate_speed(lat0, lat1, lon0, lon1, dt)
             if k==0:
                 speed = new_speed
             else:
                 speed = np.append((speed,new_speed))
+        # Account for cases where make_same_size has been used to sppend arrays
+        if lon[i][-1] == 0:
+            speed[-1] = 0
+        if i==0:
+            speed_array = speed;
+        else:
+            speed_array = np.vstack([speed_array,speed])
 
+    plot_storm_speed(speed_array,ft,lat,lon,monthday,times)
 
 if __name__ == '__main__':
     main()
